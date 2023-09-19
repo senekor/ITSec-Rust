cadc7c924efcc0cece773dce70b00a4c6a08758a
Remo Senekowitsch <remo@buenzli.dev>
2023-09-19 14:14:20 +0200

    add complicated handler exampler

--- axum-hello-world/src/main.rs
+++ axum-hello-world/src/main.rs
@@ -1,8 +1,14 @@
-use axum::{response::Html, routing::get, Router};
+#![allow(unused)]
+
+use axum::{extract::Path, response::Html, routing::get, Json, Router};
+use serde::{Deserialize, Serialize};
+use serde_json::{json, Value};
 
 #[tokio::main]
 async fn main() {
-    let hello_world_router = Router::new().route("/", get(hello_world_handler));
+    let hello_world_router = Router::new()
+        .route("/", get(hello_world_handler))
+        .route("/:my_param", get(complicated_handler));
 
     axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
         .serve(hello_world_router.into_make_service())
@@ -12,4 +18,26 @@ async fn main() {
 
 async fn hello_world_handler() -> Html<&'static str> {
     Html("<h1>Hello, World!</h1>")
+}
+
+#[derive(Deserialize)]
+struct IncomingData {
+    some_int: i32,
+    some_string: String,
+}
+
+#[derive(Serialize)]
+struct OutgoingData {
+    whatever: Vec<u8>,
+}
+
+/// Router::new().route("/:my_param", get(complicated_handler))
+///
+async fn complicated_handler(
+    Path(my_param): Path<u32>,
+    Json(incoming_data): Json<IncomingData>,
+) -> Json<OutgoingData> {
+    Json(OutgoingData {
+        whatever: vec![1, 2, 3],
+    })
 }
