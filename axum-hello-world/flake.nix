7ba5586b05ce1ab09db3c3f4864c357d5a9842fb
Sebastian Hugentobler <sebastian@vanwa.ch>
2023-10-01 12:52:06 +0200

    Nix flake now builds the musl target properly.
    Setting the CC env var in shellHook and preBuild is essential for this.

--- axum-hello-world/flake.nix
+++ axum-hello-world/flake.nix
@@ -1,45 +1,110 @@
 {
-  description = "dist doc build environment";
-  nixConfig.bash-prompt = "[nix(dist doc)] ";
+  description = "A simple flake with a devShell";
+
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/23.05";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     flake-utils.url = "github:numtide/flake-utils";
+    gitignore = {
+      url = "github:hercules-ci/gitignore.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
+  outputs = { self, nixpkgs, gitignore, flake-utils, fenix, naersk, ... }:
+    let
+      inherit (gitignore.lib) gitignoreSource;
+
+      muslConfig = pkgs:
+        let
+          ccExport = ''
+            export CC=x86_64-unknown-linux-musl-gcc
+          '';
+        in
+        (with pkgs; {
+          CARGO_BUILD_RUSTFLAGS = "-C target-feature=+crt-static";
+          shellHook = ccExport;
+          preBuild = ccExport;
+          depsBuildBuild = [ stdenv.cc ];
+        });
+
+      mkToolchain = import ./rust-toolchain.nix fenix;
+
+      mkDevShell = buildPlatform: targetPlatform: pkgs: shell:
+        let
+          rust-toolchain = mkToolchain.fromTarget {
+            inherit pkgs buildPlatform targetPlatform;
+          };
+        in
+        pkgs.mkShell
+          {
+            CARGO_BUILD_TARGET = targetPlatform;
+
+            buildInputs = with pkgs; [
+              rust-toolchain
+            ];
+          } // shell;
+
+
+      mkDevShells = buildPlatform:
+        let
+          pkgs = import nixpkgs { system = buildPlatform; };
+          defaultShell = mkDevShell buildPlatform null pkgs { };
+        in
+        {
+          default = defaultShell;
+          musl = mkDevShell buildPlatform "x86_64-unknown-linux-musl" pkgs.pkgsStatic (muslConfig pkgs.pkgsStatic);
         };
-        darwin_pkgs = if pkgs.system == "x86_64-darwin" || pkgs.system == "aarch64-darwin" then [ pkgs.darwin.apple_sdk.frameworks.Security ] else [ ];
-      in
-      with pkgs;
-      {
-        devShells.default = mkShell {
-          buildInputs = darwin_pkgs ++ [
-            gnumake
-            openssl
-            pkg-config
-            binaryen
-            cargo-deny
-            terraform
-            openssh
-            gnutar
-            gnused
-            bash
-            sqlx-cli
-            (rust-bin.stable.latest.default.override
-              {
-                targets = [
-                  "wasm32-unknown-unknown"
-                  "x86_64-unknown-linux-musl"
-                ];
-              })
-          ];
+
+      mkPackage = pkgName: pkgs: buildPlatform: targetPlatform: package:
+        let
+          toolchain = mkToolchain.fromTarget {
+            inherit pkgs buildPlatform targetPlatform;
+          };
+
+          naersk' = naersk.lib.${buildPlatform}.override
+            {
+              cargo = toolchain;
+              rustc = toolchain;
+            };
+
+          package' = {
+            name = pkgName;
+            src = gitignoreSource ./.;
+
+            doCheck = true;
+            buildInputs = with pkgs; [ toolchain ];
+          } // pkgs.lib.optionalAttrs (!isNull targetPlatform) {
+            CARGO_BUILD_TARGET = targetPlatform;
+          } // package;
+        in
+        naersk'.buildPackage package';
+
+      mkPackages = buildPlatform: pkgName:
+        let
+          pkgs = import nixpkgs { system = buildPlatform; };
+          mkPackageWithTarget = mkPackage pkgName pkgs buildPlatform;
+          defaultPackage = mkPackage pkgName pkgs buildPlatform null { };
+        in
+        {
+          default = defaultPackage;
+          musl = mkPackage pkgName pkgs.pkgsStatic buildPlatform "x86_64-unknown-linux-musl" (muslConfig pkgs.pkgsStatic);
         };
-      }
-    );
+    in
+
+    flake-utils.lib.eachDefaultSystem (system: {
+      devShells = mkDevShells system;
+      packages = mkPackages system "axum-hello-world";
+    });
 }
